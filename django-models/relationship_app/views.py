b4db71698a20432d84cd524618ff396d5c0edcbc
frankassafuah <frankassafuah@gmail.com>
2024-08-18 14:33:58 +0000

    Implementing User Authentication in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import views as auth_views
 from .models import Book
 from .models import Library
 
@@ -13,3 +15,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class LoginView(auth_views.LoginView):
+    template_name='authentication/login.html'
+
+
+class LogoutView(auth_views.LogoutView):
+    template_name='authentication/logout.html'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authentication/register.html', {'form': form})
