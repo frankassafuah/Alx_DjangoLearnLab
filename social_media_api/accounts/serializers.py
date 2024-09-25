aa129d259818785e3a012041f7387b219b319bc4
frankassafuah <frankassafuah@gmail.com>
2024-09-25 15:09:39 +0000

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,37 @@
+# accounts/serializers.py
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
-class RegisterSerializer(serializers.ModelSerializer):
-    # Explicitly define 'username' and 'password' fields using CharField
-    username = serializers.CharField(max_length=150)
-    password = serializers.CharField(write_only=True, min_length=8)
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'password', 'email')
+        model = User
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        # Create user with validated data
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data['email']
-        )
-        # Create a token for the new user
-        Token.objects.create(user=user)
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username = serializers.CharField(required=True)
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        return token
