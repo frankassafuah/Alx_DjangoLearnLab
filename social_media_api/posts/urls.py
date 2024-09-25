8f73f6674840be5f59c627931654aa0172350abb
frankassafuah <frankassafuah@gmail.com>
2024-09-25 14:53:02 +0000

    social media api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import PostListCreateView, PostDetailView
+from .views import FeedView  # Import your feed view
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('feed/', FeedView.as_view(), name='feed'),  # Add the feed endpoint
+
 ]
