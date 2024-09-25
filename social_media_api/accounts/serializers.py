342f9a0031ddb64b4b692dc038be3d2eac393fc8
frankassafuah <frankassafuah@gmail.com>
2024-09-25 15:12:51 +0000

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,12 +2,12 @@
 
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token, serializers.CharField(), Token.objects.create, get_user_model().objects.create_use
+from rest_framework.authtoken.models import Token, get_user_model().objects.create_user, get_user_model().objects.create_use
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
