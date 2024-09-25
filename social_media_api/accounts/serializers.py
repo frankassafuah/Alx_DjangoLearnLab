9f5445a1423062e44dbc74970ae523bf1caa9589
frankassafuah <frankassafuah@gmail.com>
2024-09-25 15:11:29 +0000

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token, serializers.CharField(), Token.objects.create, get_user_model().objects.create_use
 
 User = get_user_model()
 
