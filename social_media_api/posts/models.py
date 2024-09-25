de34f377b11fd2cb9d27dec5d5c89e482cfb8ac5
frankassafuah <frankassafuah@gmail.com>
2024-09-25 14:59:40 +0000

    social media api

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -13,7 +13,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user', 'post')  # Prevents multiple likes from the same user on the same post
+        
 # Explicit definition of Comment model
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
